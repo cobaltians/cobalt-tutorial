6f4d06c3c2079fb540255499ed2abb63d9d00581
Guillaume Gendre <dzb.rtz@gmail.com>
2017-03-15 20:39:01 +0100

    cleaning data and styles for ios

--- Android/app/src/main/assets/common/js/defaultData.js
+++ Android/app/src/main/assets/common/js/defaultData.js
@@ -3,119 +3,102 @@ var defaultData = [
     id: 0,
     title: "Sebastien F. birthday",
     date: '1972-11-26',
-    place: "Alsace",
-    notes: undefined
+    place: "Alsace"
   },
   {
     id: 1,
     title: "Guillaume birthday",
-    date: '1983-4-20',
-    place: "Alsace",
-    notes: undefined
+    date: '1983-04-20',
+    place: "Alsace"
   },
   {
     id: 2,
     title: "Sebastien V. birthday",
     date: '1987-12-22',
-    place: "Le Mans",
-    notes: undefined
+    place: "Le Mans"
   },
   {
     id: 3,
     title: "Kristal birthday",
-    date: '2015-2-11',
-    place: "Lannion",
-    notes: undefined
+    date: '2015-02-11',
+    place: "Lannion"
   },
   {
     id: 4,
     title: "Cobalt workshop",
-    date: '2015-4-23',
-    place: "Le Mans",
-    notes: undefined
+    date: '2015-04-23',
+    place: "Le Mans"
   },
   {
     id: 5,
     title: "Hackathon",
-    date: '2015-5-8',
-    place: "Lannion",
-    notes: undefined
+    date: '2015-05-08',
+    place: "Lannion"
   },
   {
     id: 6,
     title: "4/20 day",
     date: '2000-04-20',
-    place: "Everywhere",
-    notes: undefined
+    place: "Everywhere"
   },
   {
     id: 7,
     title: "Your cat's birthday",
-    date: '2002-8-11',
-    place: "Your Home",
-    notes: undefined
+    date: '2002-08-11',
+    place: "Your Home"
   },
   {
     id: 8,
     title: "World domination by Kristal",
     date: '2026-12-12',
-    place: "World",
-    notes: undefined
+    place: "World"
   },
   {
     id: 9,
     title: "LCS",
-    date: '2016-2-11',
-    place: "USA",
-    notes: undefined
+    date: '2016-02-11',
+    place: "USA"
   },
   {
     id: 10,
     title: "Turing birthday",
     date: '1912-06-23',
-    place: "London",
-    notes: undefined
+    place: "London"
   },
   {
     id: 11,
     title: "Imitation game",
-    date: '2015-1-7',
-    place: "World",
-    notes: undefined
+    date: '2015-01-07',
+    place: "World"
   },
   {
     id: 12,
     title: "Arpanet",
     date: '1971-09-05',
-    place: "USA",
-    notes: undefined
+    place: "USA"
   },
   {
     id: 13,
     title: "Richard Stallman birthday",
-    date: '1953-3-16',
-    place: "Manhattan",
-    notes: undefined
+    date: '1953-03-16',
+    place: "Manhattan"
   },
   {
     id: 14,
     title: "Transitor invention",
     date: '1947-12-23',
-    place: "USA",
-    notes: undefined
+    place: "USA"
   },
   {
     id: 15,
     title: "Apple fundation",
-    date: '1976-4-1',
-    place: "California",
-    notes: undefined
+    date: '1976-04-01',
+    place: "California"
   },
   {
     id: 16,
     title: "Linux",
-    date: '1991-8-26',
-    place: "USA",
-    notes: undefined
+    date: '1991-08-26',
+    place: "USA"
   }
 ];
